a56dc57a0d7757e8bfd17e7915eae078259cc70a
Fabrice Baumann <fa.baumann@gmail.com>
2014-09-14 00:57:03 +0200

    Update the webp conversion script

--- webp.sh
+++ webp.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 # 1 arg - prog (cwebp|gif2webp), 2 arg - folder full path, if those 2 args are not passed we exit
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export PATH=$DIR:$PATH
 
 if [ -z "${1}" -o -z "${2}" ]; then 
+    echo "Unsufficient information. Exit"
     exit
 fi
 
 if [ "$1" == "jpg"  ]; then
-    find "$2" -iregex ".*\.\(jpe?g\)" -exec bash -c 'for x; do x=${x#./}; ./cwebp -mt -m 6 "$x" -o "$x.webp"; done' _ {} +
+    echo "Compressing jpg to webp";
+    find "$2" -iregex ".*\.\(jpe?g\)" -exec bash -c 'for x; do x=${x#./}; cwebp -af -metadata none -mt -m 6 "$x" -o "$x.webp"; done' _ {} +;
 fi
 
 if [ "$1" == "png"  ]; then
-    find "$2" -iregex ".*\.\(png\)" -exec bash -c 'for x; do x=${x#./}; ./cwebp -mt -m 6 "$x" -o "$x.webp"; done' _ {} +
+    echo "Compressing png to webp";
+    find "$2" -iregex ".*\.\(png\)" -exec bash -c 'for x; do x=${x#./}; cwebp -af -metadata none -mt -m 6 "$x" -o "$x.webp"; done' _ {} +;
 fi
 
 if [ "$1" == "gif"  ]; then
-    find "$2" -iregex ".*\.\(gif\)" -exec bash -c 'for x; do x=${x#./}; ./gif2webp -mt -m 6 "$x" -o "$x.webp"; done' _ {} +;
+    echo "Compressing gif to webp";
+    find "$2" -iregex ".*\.\(gif\)" -exec bash -c 'for x; do x=${x#./}; gif2webp -mt -m 6 "$x" -o "$x.webp"; done' _ {} +;
 fi
 
